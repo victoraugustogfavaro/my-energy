851c0d159ba73d2a81486b240ad48e6befec9783
felipeclarindo <fglpc2@gmail.com>
2024-11-20 01:17:01 -0300

    Melhorias no Banner e Menu

--- my-energy/src/app/page.tsx
+++ my-energy/src/app/page.tsx
@@ -4,6 +4,64 @@ function Home() {
   return (
     <main>
       <Banner />
+      <div className={"w-[70%] my-[3%] mx-auto"}>
+        <h2 className={"text-laranja mt-[30px] text-[1.6rem] font-bold"}>
+          Nossa Essência
+        </h2>
+        <p className={"mt-[14px] text-[1.2rem] text-cinza"}>
+          Acreditamos que entender o{" "}
+          <span className="font-bold">impacto do consumo de energia</span> é o
+          primeiro passo para um mundo mais sustentável. Nosso gerenciador
+          permite que você{" "}
+          <span className="font-bold">visualize as emissões de CO₂</span>{" "}
+          geradas por cada conta de luz, incentivando escolhas mais conscientes
+          e{" "}
+          <span className="font-bold">
+            contribuindo para a preservação do meio ambiente
+          </span>
+          .
+        </p>
+        <h2 className={"text-laranja mt-[30px] text-[1.6rem] font-bold"}>
+          Por que Energia Sustentável?
+        </h2>
+        <p className={"mt-[14px] text-[1.2rem] text-cinza"}>
+          A energia sustentável é essencial para{" "}
+          <span className="font-bold">combater as mudanças climáticas</span> e{" "}
+          <span className="font-bold">
+            reduzir a dependência de combustíveis fósseis
+          </span>
+          . Fontes como solar e eólica emitem significativamente menos CO₂,
+          preservando a qualidade do ar e os ecossistemas.
+        </p>
+        <p className={"mt-[14px] text-[1.2rem] text-cinza"}>
+          Nosso projeto busca incentivar o uso dessas fontes renováveis ao
+          tornar visível o impacto do consumo atual de cada um, promovendo a{" "}
+          {""}
+          <span className="font-bold">conscientização</span> sobre as
+          alternativas energéticas mais limpas.
+        </p>
+        <h2 className={"text-laranja mt-[30px] text-[1.6rem] font-bold"}>
+          Benefícios do Monitoramento do Consumo de Energia
+        </h2>
+        <p className={"mt-[14px] text-[1.2rem] text-cinza"}>
+          Com o gerenciador da MyEnergy, você acompanha o impacto ambiental de
+          cada kWh consumido. Você pode observar como pequenas mudanças no
+          consumo podem{" "}
+          <span className="font-bold">reduzir as emissões de CO₂</span>. Esse
+          monitoramento é uma ferramenta poderosa para quem quer economizar e
+          adotar práticas mais ecológicas.
+        </p>
+        <p className={"mt-[14px] text-[1.2rem] text-cinza"}>
+          Cada ajuste no seu consumo não só contribui para a redução dos custos
+          de energia, mas também representa um passo em direção a um{" "}
+          <span className="font-bold">futuro mais limpo</span>. Ao ter
+          visibilidade sobre o impacto de cada hábito, você é capacitado a
+          adotar uma postura mais <span className="font-bold">consciente</span>{" "}
+          e <span className="font-bold">sustentável</span>, promovendo um efeito
+          positivo para a{" "}
+          <span className="font-bold">preservação do planeta!</span>
+        </p>
+      </div>
     </main>
   );
 }
--- my-energy/src/components/Banner/Banner.tsx
+++ my-energy/src/components/Banner/Banner.tsx
@@ -4,15 +4,16 @@ import Link from "next/link";
 
 function Banner() {
   return (
-    <section className="flex flex-col bg-gradientFundo text-white text-center h-auto gap-6 p-5 justify-center items-center md:gap-10 xl:flex-row xl:text-left xl:justify-evenly xl:p-10 xl:gap-8">
-      <div className="flex flex-col justify-center items-center gap-6 md:w-[600px] xl:w-[468px] xl:ml-5">
-        <h1 className="font-bold text-[1.7rem] md:text-[2rem] xl:text-[2.625rem] text-center">
+    <section className="flex flex-col bg-gradientFundo text-white text-center h-auto gap-6 p-5 justify-center items-center md:gap-10 xl:flex-row xl:text-left xl:justify-evenly xl:p-9 xl:gap-8">
+      <div className="flex flex-col justify-center items-center gap-6 md:w-[610px] xl:w-[500px] xl:ml-5">
+        <h1 className="font-bold text-[1.7rem] md:text-[2rem] xl:text-[2.4rem] text-center">
           Sustentabilidade para um futuro melhor.
         </h1>
         <h2 className="text-[1.1rem] md:text-[1.2rem] text-center">
-          Na MyEnergy, acreditamos que a mudança começa com a conscientização.
-          Nosso projeto visa capacitar cada pessoa a entender melhor seu consumo
-          de energia e seu impacto ambiental.
+          Na MyEnergy, acreditamos que a mudança começa com a{" "}
+          <span className="font-bold">conscientização.</span> Nosso projeto visa
+          capacitar cada pessoa a entender melhor seu consumo de energia e seu{" "}
+          <span className="font-bold">impacto ambiental.</span>
         </h2>
         <Link
           href="/contas"
